b02fe0b2fcfdf764a8b8c7f445302fb0aea52b0b
huyduong1589 <huyduong1589@gmail.com>
2019-04-08 11:45:58 +0800

    update

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' , message: 'This is the first page!'});
+  res.render('index', { title: 'Express' , message: 'This is the second page!'});
 });
 
 module.exports = router;
